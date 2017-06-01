eda3bb2047da3cfd87a14e36b342514a7a02ef28
fredix <fredix@protonmail.com>
2017-06-01 22:41:17 +0200

    added drone-wall to socker stack

--- drone/drone-wall.sh
+++
@@ -1,1 +0,0 @@
-docker service create --name drone-wall --constraint 'node.labels.location == home' --network traefik-net --label traefik.frontend.rule=Host:drone-wall.fredix.xyz --label traefik.port=80 drone/drone-wall
